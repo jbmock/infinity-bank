9c49b7c0ea868adc3a4df22da6bcf356188a98c0
Jess, she/her <113154339+jbmock@users.noreply.github.com>
2023-06-30 18:36:09 -0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+import 'bootstrap/dist/css/bootstrap.css';
+
+//routes & components
+//import ErrorPage from './error-page';
+import TransactionForm from './routes/transactionForm';
+import CreateAccount from './routes/createaccount';
+import Home from './routes/home';
+import Login from './routes/login';
+import NavBar from './components/navbar';
+import Transactions from './routes/transactions';
+
+function App() {
+    const {user} = useAuthContext()
+
+    return (
+        <BrowserRouter>
+        <NavBar/>
+            <Routes>
+                <Route 
+                    path="/" 
+                    exact element={<Home/>} 
+                />
+                {/* <Route 
+                    path="/" 
+                    element={<root />} 
+                    errorElement={<ErrorPage/>} 
+                /> */}
+                <Route 
+                    path="/CreateAccount" 
+                    element={!user ? <CreateAccount/> : <Navigate to="/" />} 
+                />
+                <Route 
+                    path="/login" 
+                    element={!user ? <Login/> : <Navigate to="/" />} 
+                />
+                <Route 
+                    path="/atm" 
+                    element={user ? <TransactionForm/> : <Navigate to="/login" />}
+                />
+                <Route 
+                    path="/transactions" 
+                    element={user ? <Transactions/> : <Navigate to="/login" />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
