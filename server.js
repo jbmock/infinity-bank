ec3b29bd7d0f840333a5a88fb804fb31d07ec956
Jess, she/her <113154339+jbmock@users.noreply.github.com>
2023-06-30 22:21:46 -0500

    trial: commenting out static server wild card routes again

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // middleware
 app.use(cors());
