e62699c04cdf0314d5aa705b770b5ac482cccb87
Jess, she/her <113154339+jbmock@users.noreply.github.com>
2023-07-01 18:15:56 -0500

    trial: changed port to same port Heroku gives (otherwise receive an unauthorized error)

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.MONGO_URI, {
 })
    .then(() => {
     // listen for requests
-    app.listen(process.env.PORT || 4000, () => {
+    app.listen(process.env.PORT || 5226, () => {
         console.log('connected to db & listening on port', process.env.PORT)
        })
    })
