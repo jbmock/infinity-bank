1cd4f2813e444eddc3af05b76cd5107e8a8ab225
Jess, she/her <113154339+jbmock@users.noreply.github.com>
2023-07-01 18:53:17 -0500

    error: unauthorized 401 error in Heroku app trial: removed Heroku url in useSignup and useLogin hooks

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('https://jessica-mock-fullstackbanking-f5b46f56980a.herokuapp.com/api/user/login', {
+        const response = await fetch('/api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
