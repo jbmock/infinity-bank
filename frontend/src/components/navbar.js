f678fefaa336b57882976ad2d86642a8069d46dd
Jess, she/her <113154339+jbmock@users.noreply.github.com>
2023-07-02 14:12:44 -0500

    removing unneeded <ul> and <li> in navbar and removing collapse feature since I can't get it to work

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -18,24 +18,20 @@ export default function NavBar() {
         <Link className="navbar-brand" to="/">
         <img src={logo} alt="Logo" width="35" height="35" className="d-inline-block align-text-top"/>Infinity Bank
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> */}
+        {/* <span className="navbar-toggler-icon"></span> */}
+        {/* </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarNavAltMarkup"> */}
             <div className="main-nav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
+                <div className="navbar-nav">
                     <Link className="nav-link" to="/atm">ATM
                     <p>Make deposits & withdraws + view my current balance</p></Link>
-                    </li>
-                    <li className="nav-item">
                     <Link className="nav-link" to="/transactions">Transaction History
                     <p>View my transaction history</p></Link>
-                    </li>
-                </ul>
+                </div>
             </div>
             <div className="nav-right">
-                <ul className="navbar-nav">
+                <div className="navbar-nav">
                     {user && (
                         <div className="greeting">
                             <span>Welcome, {user.email}!</span>
@@ -44,19 +40,15 @@ export default function NavBar() {
                     )}
                     {!user && (
                         <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <li className="nav-item">
                                 <Link className="nav-link" to="/CreateAccount">Create Account
                                 <p>Create a new account</p></Link>
-                            </li>
-                            <li className="nav-item">
                                 <Link className="nav-link" to="/login">Login
                                 <p>Sign in to my account</p></Link>
-                            </li>
                         </div>
                     )}
-                </ul>
+                </div>
             </div>
             </div>
-    </div>
+    {/* </div> */}
 </nav>
 )}
