b06b197c7e2d699e093c86a6acdcc38aaf467576
Jess, she/her <113154339+jbmock@users.noreply.github.com>
2023-07-02 20:16:37 -0500

    Heroku threw not found error. trial: replaced Heroku URL w/ localhost:4000

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('https://jessica-mock-fullstackbanking-f5b46f56980a.herokuapp.com/api/user/signup', {
+        const response = await fetch('http://localhost:4000/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
